66f3b1cec042a22f8e4719daf21fc0a3e7851290
cjcj5438 <1002931123@qq.com>
2020-01-10 09:41:07 +0800

    init19

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,10 +6,10 @@ src=$(pwd)"/"
 des=/usr/chenjing/
 now=$(date +"%Y-%m-%d %H:%M:%S")
 
-rsync -vzrc --delete --exclude ".git" --exclude ".env" --exclude ".circleci" $src $user@$host:$des
+#rsync -vzrc --delete --exclude ".git" --exclude ".env" --exclude ".circleci" $src $user@$host:$des
 
-ssh $user@$host "sudo chown -R www:www $des"
-
+ssh $user@$host "sudo chown -R $des"
+echo "已经登录"
 ssh $user@$host "chmod -R 775 $des/bootstrap/cache && chmod -R 775 $des/storage && cd $des && pwd && ls -al"
 
 echo "$now update $host $des code"
