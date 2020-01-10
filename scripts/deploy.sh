99b4e16d98ccac555cc5f5fa97a7a51e851593e5
cjcj5438 <1002931123@qq.com>
2020-01-10 09:38:04 +0800

    init17

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,38 +1,15 @@
-#!/bin/sh
-# 构想 https://gist.github.com/motemen/8595451
+#!/usr/bin/env bash
 
-# 基于 https://github.com/eldarlabs/ghpages-deploy-script/blob/master/scripts/deploy-ghpages.sh
-# MIT许可 https://github.com/eldarlabs/ghpages-deploy-script/blob/master/LICENSE
+user=root
+host=47.104.64.78
+src=$(pwd)"/"
+des=/usr/chenjing/
+now=$(date +"%Y-%m-%d %H:%M:%S")
 
-# abort the script if there is a non-zero error
-set -e
+rsync -vzrc --delete --exclude ".git" --exclude ".env" --exclude ".circleci" $src $user@$host:$des
 
-# 打印当前的工作路径
-pwd
+ssh $user@$host "sudo chown -R www:www $des"
 
-user="root"
-passwd="CHENjing@10502"
+ssh $user@$host "chmod -R 775 $des/bootstrap/cache && chmod -R 775 $des/storage && cd $des && pwd && ls -al"
 
-host_list=("47.104.64.78")
-
-#seletc the host
-select host in ${host_list[@]}; do
-  break
-done
-
-#execute the ssh action
-
-echo "开始连接服务器:"
-expect -c "
-spawn ssh $user@$host
-expect {
-  \"yes\/no\" { send \"yes\n\"; exp_continue}
-  \"password:\" { send \"$passwd\n\" }
-}
-interact
-"
-echo "登录服务器成功"
-pwd
-cd /usr/local/nginx/htm
-
-echo "Finished Deployment!"
+echo "$now update $host $des code"
