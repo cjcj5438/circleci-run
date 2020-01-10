d3356fd8567fac19885004e47cb5e445c2e389c2
cjcj5438 <1002931123@qq.com>
2020-01-10 13:09:59 +0800

    init29

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# 构想 https://gist.github.com/motemen/8595451
+#  构想 https://gist.github.com/motemen/8595451
 
 # 基于 https://github.com/eldarlabs/ghpages-deploy-script/blob/master/scripts/deploy-ghpages.sh
 # MIT许可 https://github.com/eldarlabs/ghpages-deploy-script/blob/master/LICENSE
